a14e8fe84f0cdcadeaacb5c730d5dfbbfa108112
zlatozar <zlatozarpoptolev@gmail.com>
2023-07-12 15:29:31 +0300

    Add base methods

--- src/Aibest.Business/Services/ResumeService.cs
+++ src/Aibest.Business/Services/ResumeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Aibest.Business.Models;
+using Aibest.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,74 +11,186 @@ namespace Aibest.Business.Services
 {
     public class ResumeService : IResumeService
     {
+        private readonly ApplicationDbContext context;
+
+        public ResumeService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
         public bool AddCertificate(int resumeId, string name, DateTime issuedYear, string description)
         {
-            throw new NotImplementedException();
+            var certificate = new CertificateModel()
+            {
+                ResumeId = resumeId,
+                Name = name,
+                IssuedYear = issuedYear,
+                Description = description
+            };
+            context.Certificates.Add(certificate);
+            context.SaveChanges();
         }
 
         public bool AddEducation(int resumeId, string name, string country, string major, DateTime beginYear, DateTime endYear)
         {
-            throw new NotImplementedException();
+            var education = new EducationModel()
+            {
+                ResumeId = resumeId,
+                Name = name,
+                Country = country,
+                Major = major,
+                BeginYear = beginYear,
+                EndYear = endYear
+            };
+            context.Educations.Add(education);
+            context.SaveChanges();
         }
 
         public bool AddJob(string name, int resumeId, string companyName, string position, string description, DateTime beginYear, DateTime endYear)
         {
-            throw new NotImplementedException();
+            var job = new JobModel()
+            {
+                ResumeId = resumeId,
+                Name = name,
+                CompanyName = companyName,
+                Position = position,
+                Description = description,
+                BeginYear = beginYear,
+                EndYear = endYear
+            };
+            context.Jobs.Add(job);
+            context.SaveChanges();
         }
 
         public bool AddLanguage(int resumeId, string name, string level)
         {
-            throw new NotImplementedException();
+            var language = new LanguageModel()
+            {
+                ResumeId = resumeId,
+                Name = name,
+                Level = level
+            };
+            context.Languages.Add(language);
+            context.SaveChanges();
         }
 
         public bool AddSkillToResume(int resumeId, string name)
         {
-            throw new NotImplementedException();
+            var skill = new SkillModel()
+            {
+                ResumeId = resumeId,
+                Name = name
+            };
+            context.Skills.Add(skill);
+            context.SaveChanges();
+
         }
 
         public bool CreateResume(string resumeName, int userId)
         {
-            throw new NotImplementedException();
+            var resume = new ResumeModel()
+            {
+                Name = resumeName,
+                UserId = userId
+            };
+            context.Resumes.Add(resume);
+            context.SaveChanges();
+            
         }
 
         public bool DeleteResume(int resumeId)
         {
-            throw new NotImplementedException();
+            var resume = context.Resumes.FirstOrDefault(r => r.Id == resumeId);
+            if (resume != null)
+            {
+                context.Resumes.Remove(resume);
+                context.SaveChanges();
+                return true;
+            }
+
         }
 
         public bool GetResumeById(int resumeId)
         {
-            throw new NotImplementedException();
-        }
+            var resume = context.Resumes.FirstOrDefault(r => r.Id == resumeId);
+                }
 
         public bool RemoveCertificate(int resumeId, int certificateId)
         {
-            throw new NotImplementedException();
+            var certificate = context.Certificates.FirstOrDefault(c => c.Id == certificateId && c.ResumeId == resumeId);
+            if (certificate != null)
+            {
+                context.Certificates.Remove(certificate);
+                context.SaveChanges();
+                return true;
+            }
+            return false;
         }
 
         public bool RemoveEducation(int resumeId, int educationId)
         {
-            throw new NotImplementedException();
+            var education = context.Educations.FirstOrDefault(e => e.Id == educationId && e.ResumeId == resumeId);
+            if (education != null)
+            {
+                context.Educations.Remove(education);
+                context.SaveChanges();
+                return true;
+            }
+            return false;
         }
 
         public bool RemoveJob(int resumeId, int jobId)
         {
-            throw new NotImplementedException();
+            var job = context.Jobs.FirstOrDefault(j => j.Id == jobId && j.ResumeId == resumeId);
+            if (job != null)
+            {
+                context.Jobs.Remove(job);
+                context.SaveChanges();
+                return true;
+            }
+            return false;
+            
         }
 
         public bool RemoveLanguage(int resumeId, int languageId)
         {
-            throw new NotImplementedException();
+            var language = context.Languages.FirstOrDefault(l => l.Id == languageId && l.ResumeId == resumeId);
+            if (language != null)
+            {
+                context.Languages.Remove(language);
+                context.SaveChanges();
+                return true;
+            }
+            return false;
         }
 
         public bool RemoveSkillFromResume(int resumeId, int skillId)
         {
-            throw new NotImplementedException();
+            var skill = context.Skills.FirstOrDefault(s => s.Id == skillId && s.ResumeId == resumeId);
+            if (skill != null)
+            {
+                context.Skills.Remove(skill);
+                context.SaveChanges();
+                return true;
+            }
+            return false;
         }
 
         public bool UpdateResume(string FirstName, string MiddleName, string LastName, string Email, string PhoneNumber, string Address)
         {
-            throw new NotImplementedException();
+            var resume = context.Resumes.FirstOrDefault(r => r.Id == resumeId);
+            if (resume != null)
+            {
+                resume.FirstName = FirstName;
+                resume.MiddleName = MiddleName;
+                resume.LastName = LastName;
+                resume.Email = Email;
+                resume.PhoneNumber = PhoneNumber;
+                resume.Address = Address;
+                context.SaveChanges();
+                return true;
+            }
+            return false;
         }
     }
 }
