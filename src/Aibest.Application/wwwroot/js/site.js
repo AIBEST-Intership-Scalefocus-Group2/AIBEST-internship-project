67f206c0136c881e36cbaa2303273ec5600c1f61
zlatozar <zlatozarpoptolev@gmail.com>
2023-07-19 14:54:40 +0300

    Add base pdf download functionality

--- src/Aibest.Application/wwwroot/js/site.js
+++ src/Aibest.Application/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$("document").ready(function () {
+    var doc = new jsPDF('p', 'pt', 'A4');
+
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
+        }
+    };
+
+    $('#generate-pdf').on('click', function () {
+        doc.fromHTML($('#cv-content').html(), 30, 15, {
+            'width': 2500,
+            'elementHandlers': specialElementHandlers
+        });
+        doc.save('resume.pdf');
+    })
+});
+
